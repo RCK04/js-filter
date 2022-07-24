da0d7ea6c566c8d5f9031c28fc4149bdc411d207
Erick Roberto <erickkan2004@gmail.com>
2022-07-24 16:23:08 -0300

    git commit -m "mensagem"

---
+++ index.js
@@ -0,0 +1,17 @@
+const users = [
+    { name: 'Ada Lovelace', premium: true },
+    { name: 'Grace Hopper', premium: false },
+    { name: 'Alan Turing', premium: true },
+    { name: 'Linus Torvalds', premium: false },
+    { name: 'Margaret Hamilton', premium: true },
+    { name: 'Carlos Magnum' , premium: false},
+    { name: 'Carlos Magnum' , premium: false},
+    { name: 'Carlos Magnum' , premium: false},
+    { name: 'Carlos Magnum' , premium: true},
+    { name: 'Carlos Magnum' , premium: true},
+    { name: 'Carlos Magnum' , premium: true}
+  ]
+
+  const premiumUsers = users.filter(user => user.premium)
+
+  console.log(premiumUsers)
